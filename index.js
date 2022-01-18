692012e8ef695ba51680fb904ff1c7a7c385c95e
Gustavo Freitas <89526443+GustavoFreitas10@users.noreply.github.com>
2022-01-18 14:55:42 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,19 +2,19 @@ const express = require("express");
 const { MongoClient, ObjectId } = require("mongodb");
 
 const url = "mongodb://localhost:27017";
-// const url = "mongodb+srv://admin:hd2rV5duoPrrIi3t@cluster0.jup2c.mongodb.net/";
+const url = "mongodb+srv://guh1:Timaki123@@cluster0.kvjz9.mongodb.net/";
 
 const dbName = "ocean_bancodados_18_01_2022";
 
 async function main() {
     // Conexão com o Banco de Dados
-/*
+    
     const client = await MongoClient.connect(url);
 
     const db = client.db(dbName);
 
     const collection = db.collection("herois");
-/*
+    
     // Aplicação em Express
 
     const app = express();
